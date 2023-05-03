9176b79fa4835f95c94a4a74619c7f418b43aa34
nicolas.bone <nicobone4021@hotmail.com>
2023-05-02 22:29:47 -0300

    Subo todo el tp

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3.9-slim-bullseye
+WORKDIR /sim
+
+RUN apt-get update && apt-get upgrade -y && apt-get install -y python3-dev default-libmysqlclient-dev build-essential
+
+COPY sim ./
+COPY requirements.txt ./
+
+RUN pip install --upgrade pip && pip install -r requirements.txt
+
+ENTRYPOINT ["python3"]
+CMD ["manage.py", "runserver", "0.0.0.0:8000"]
---
+++ sim/tp3/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from .forms import Tp3Form
+from .funciones import simular_e1, simular_e2
+
+def index_view(request):
+    return render(request, 'index.html')
+
+def tp3_view(request):
+    if request.method == "POST":
+        formulario = Tp3Form(request.POST)
+        if formulario.is_valid():
+            cantidad_simulaciones = formulario.cleaned_data["cantidad_simulaciones"]
+            mostrar_desde = formulario.cleaned_data["mostrar_desde"]
+            cuantas_mostrar = formulario.cleaned_data["cuantas_mostrar"]
+            media = formulario.cleaned_data["media"]
+            desviacion = formulario.cleaned_data["desviacion"]
+            contexto1 = simular_e1(cantidad_simulaciones, mostrar_desde, cuantas_mostrar, media, desviacion)
+            #contexto2 = simular_e2(cantidad_simulaciones, mostrar_desde, cuantas_mostrar, media, desviacion)
+            return render(request, 'tp3.html', {'form': formulario, "contexto1": contexto1})
+            #return render(request, 'tp3.html', {'form': formulario, "contexto1": contexto1, 'contexto2': contexto2})
+            #return render(request, 'tp3.html', {'form': formulario, 'contexto2': contexto2})
+    else:
+        formulario = Tp3Form()
+        return render(request, 'tp3.html', {'form': formulario, 'error': "a"})
