8f48847019a43cd416cf696b730445c6b0841fc7
nicolas.bone <nicobone4021@hotmail.com>
2023-06-22 16:54:06 -0300

    Final

--- sim/tp4/forms.py
+++ sim/tp4/forms.py
@@ -3,8 +3,11 @@ from django.forms import IntegerField, TextInput, Select, Form, CharField, Model
 
 
 class Tp4Form(Form):
-    cantidad_simulaciones = IntegerField(label='Cantidad de ciclos de simulaciones', widget=TextInput(attrs={'class': 'form-control'}))
-    mostrar_desde = IntegerField(label='Desde que dia de simulación mostrar', widget=TextInput(attrs={'class': 'form-control'}))
-    cuantas_mostrar = IntegerField(label='Cuántos días mostrar?', widget=TextInput(attrs={'class': 'form-control'}))
-    media = FloatField(label="Media", widget=TextInput(attrs={'class': 'form-control'}))
-    desviacion = FloatField(label="Desviación Estándar", widget=TextInput(attrs={'class': 'form-control'}))
+    cantidad_simulaciones = IntegerField(label='Cantidad de filas a simular?', widget=TextInput(attrs={'class': 'form-control'}))
+    mostrar_desde = IntegerField(label='Desde que línea de simulación mostrar?', widget=TextInput(attrs={'class': 'form-control'}))
+    cuantas_mostrar = IntegerField(label='Cuántas líneas mostrar?', widget=TextInput(attrs={'class': 'form-control'}))
+    media_llegada_parque = FloatField(label="Media Llegada Parque(min)", widget=TextInput(attrs={'class': 'form-control'}))
+    media_llegada_caja_est = FloatField(label="Media Atención Caja Estacionamiento(min)", widget=TextInput(attrs={'class': 'form-control'}))
+    media_llegada_caja_cont = FloatField(label="Media Atención Caja Control(min)", widget=TextInput(attrs={'class': 'form-control'}))
+    media_llegada_caja_comp = FloatField(label="Media Atención Caja Entradas(min)", widget=TextInput(attrs={'class': 'form-control'}))
+    media_estacionamiento = FloatField(label="Media Tiempo de Estacionamiento", widget=TextInput(attrs={'class': 'form-control'}))
--- sim/tp4/views.py
+++ sim/tp4/views.py
@@ -12,10 +12,24 @@ def tp4_view(request):
             cantidad_simulaciones = formulario.cleaned_data["cantidad_simulaciones"]
             mostrar_desde = formulario.cleaned_data["mostrar_desde"]
             cuantas_mostrar = formulario.cleaned_data["cuantas_mostrar"]
-            media = formulario.cleaned_data["media"]
-            desviacion = formulario.cleaned_data["desviacion"]
-            a, llaves = tp4(mostrar_desde, cuantas_mostrar, cantidad_simulaciones)
-            return render(request, 'tp4.html', {'form': formulario, "contexto1": a, "llaves": llaves})
+            media_llegada_parque = formulario.cleaned_data["media_llegada_parque"]
+            media_llegada_caja_est = formulario.cleaned_data["media_llegada_caja_est"]
+            media_llegada_caja_cont = formulario.cleaned_data["media_llegada_caja_cont"]
+            media_llegada_caja_comp = formulario.cleaned_data["media_llegada_caja_comp"]
+            media_estacionamiento = formulario.cleaned_data["media_estacionamiento"]
+            a, llaves, rta = tp4(mostrar_desde,
+                            cuantas_mostrar,
+                            cantidad_simulaciones,
+                            media_estacionamiento,
+                            media_llegada_caja_est,
+                            media_llegada_caja_cont,
+                            media_llegada_caja_comp,
+                            media_llegada_parque)
+
+            rta["promedio_tiempo_estacionamiento"] = rta["ac_demora_estacionar"] / rta["ac_estacionados"] if rta["ac_estacionados"] !=0 else 0
+            rta["promedio_tiempo_control"] = rta["ac_demora_control"] / rta["ac_personas"] if rta["ac_personas"] !=0 else 0
+            rta["promedio_tiempo_compra"] = rta["ac_demora_atencion_en_caja_compra"] / rta["ac_grupos_en_caja_compra"] if rta["ac_grupos_en_caja_compra"] !=0 else 0
+            return render(request, 'tp4.html', {'form': formulario, "contexto1": a, "llaves": llaves, "rta": rta})
     else:
         formulario = Tp4Form()
         return render(request, 'tp4.html', {'form': formulario, 'error': "a"})
